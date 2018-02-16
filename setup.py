772087896b0dae62d4cdf340aadbda4129e415cf
Brad Belyeu <bradleylamar@gmail.com>
2018-02-16 09:22:29 -0600

    :heavy_plus_sign: Added Flask-Babel as a dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     license='MIT',
     author='Brad Belyeu',
     author_email='bradleylamar@gmail.com',
-    description='Add basic i18n functionality for APIs built as Flask apps.',
+    description=('Add basic i18n functionality for APIs built as Flask apps. Intended to work '
+                 'in conjuction with Flask-Babel'),
     long_description=__doc__,
     packages=['flask_i18n'],
     zip_safe=False,
@@ -17,6 +18,7 @@ setup(
     platforms='any',
     install_requires=[
         'Flask',
+        'Flask-Babel',
     ],
     classifiers=[
         'Environment :: Web Environment',
@@ -28,6 +30,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    keywords=['flask', 'i18n'],
+    keywords=['flask', 'i18n', 'l10n'],
     test_suite='flask_i18n.tests',
 )
